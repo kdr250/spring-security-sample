91a8dfc06b7f7125469716a6a7dc7764be05babf
kdr250 <54400975+kdr250@users.noreply.github.com>
2021-06-28 21:00:40 +0900

    UsernamePasswordJsonAuthenticationFilterクラス作成

---
+++ src/main/java/springsecuritysample/security/UsernamePasswordJsonAuthenticationFilter.java
@@ -0,0 +1,29 @@
+package springsecuritysample.security;
+
+import org.json.JSONObject;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+public class UsernamePasswordJsonAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            String requestBody = request.getReader().lines().collect(Collectors.joining("\r\n"));
+            JSONObject jsonObject =  new JSONObject(requestBody);
+            String username = (String) jsonObject.get("username");
+            String password = (String) jsonObject.get("password");
+            UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(username, password);
+            setDetails(request, authRequest);
+            return super.getAuthenticationManager().authenticate(authRequest);
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+}
