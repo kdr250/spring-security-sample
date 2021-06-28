53a40fcfad9db3cd9fb4e7c0a3c1ddbec10c90d5
kdr250 <54400975+kdr250@users.noreply.github.com>
2021-06-28 21:00:12 +0900

    JsonMapperTestクラス作成

---
+++ src/test/java/springsecuritysample/json/JsonMapperTest.java
@@ -0,0 +1,18 @@
+package springsecuritysample.json;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JsonMapperTest {
+    @Test
+    void JSON文字列をオブジェクトに変換できる() {
+        String json = "{\n" +
+                "    \"username\": \"akihiro\",\n" +
+                "    \"password\": \"nishikawa123\"\n" +
+                "}";
+        JSONObject jsonObject = new JSONObject(json);
+        assertEquals("akihiro", jsonObject.get("username"));
+    }
+}
