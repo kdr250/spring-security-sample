4d8861e3dbf4cf1943c3374e2855aa8606dc53f3
kdr250 <54400975+kdr250@users.noreply.github.com>
2021-07-03 11:38:27 +0900

    AuthorizationTestを作成して認可のテスト

---
+++ src/test/java/springsecuritysample/security/AuthorizationTest.java
@@ -0,0 +1,83 @@
+package springsecuritysample.security;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import springsecuritysample.security.service.UserService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class AuthorizationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        userService.signUp("taro", "password123");
+    }
+
+    @Test
+    void USER権限を持っている人はUSER権限が必要なパスにリクエストできる() throws Exception {
+        String json = "{\n" +
+                "    \"username\": \"taro\",\n" +
+                "    \"password\": \"password123\"\n" +
+                "}";
+        MvcResult loginResult = mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(json)
+        )
+                .andExpect(status().isNoContent())
+                .andReturn();
+
+        MockHttpSession session = (MockHttpSession) loginResult.getRequest().getSession();
+
+        MvcResult result = mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/user")
+                    .session(session)
+        )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        assertEquals("USER画面です", result.getResponse().getContentAsString());
+    }
+
+    @Test
+    void USER権限を持っている人はADMIN権限が必要なパスにリクエストできない() throws Exception {
+        String json = "{\n" +
+                "    \"username\": \"taro\",\n" +
+                "    \"password\": \"password123\"\n" +
+                "}";
+        MvcResult loginResult = mockMvc.perform(
+                MockMvcRequestBuilders.post("/api/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+        )
+                .andExpect(status().isNoContent())
+                .andReturn();
+
+        MockHttpSession session = (MockHttpSession) loginResult.getRequest().getSession();
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/admin")
+                        .session(session)
+        )
+                .andExpect(status().isForbidden());
+    }
+}
